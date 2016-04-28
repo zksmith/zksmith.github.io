ba212482d7e3fa328b25466a7d0e8122ce9da19b
itsBosco <zahiersmith96@gmail.com>
2016-04-28 03:25:11 -0400

    Update advice.js

--- advice.js
+++ advice.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
   $.getJSON("http://api.adviceslip.com/advice", function(json) {
-      $(".quote").html(json.slip.advice);
+    $(".twitter-share-button").attr("href", "https://twitter.com/intent/tweet?text="+json.slip.advice);
+    $(".quote").html(json.slip.advice);
   });
   $("#getMessage").on("click", function() {
+    
     $.getJSON("http://api.adviceslip.com/advice", function(json) {
       $(".quote").html(json.slip.advice);
+      $(".twitter-share-button").attr("href", "https://twitter.com/intent/tweet?text="+json.slip.advice);
     });
+    $(".twitter-share-button").attr("href", "https://twitter.com/intent/tweet?text="+json.slip.advice);
   });
 });
