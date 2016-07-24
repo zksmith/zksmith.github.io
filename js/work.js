ae1e549f517fffcbf69120e5ed26e32ebe36b82e
Bosco <zahiersmith96@gmail.com>
2016-07-24 17:23:56 -0400

    added tic tac toe

--- js/work.js
+++ js/work.js
@@ -17,6 +17,16 @@ var projects = {
             "description": "A previous version of this site",
             "image": "img/item2.png",
             "link": "http://www.zahiersmith.com/old-website-1"
+        },
+
+
+    ],
+    "projects2": [
+        {
+            "name": "Tic-Tac-Toe",
+            "description": "The classic game built using JavaScript/jQuery",
+            "image": "img/tictac.png",
+            "link": "http://www.zahiersmith.com/tictactoe"
         }
     ]
 };
@@ -25,6 +35,9 @@ $(document).ready(function () {
     projects.display = function () {
         projects.projects.forEach(function (project) {
             $(".work-row").append('<div class="four columns"><a href="' + project.link + '" target="_blank"><div class="imgWrap"><img src="' + project.image + '" /><div class="imgDescription"><h4>' + project.name + '</h4><p>' + project.description + '</p></p></div></div></div></a>');
+        });
+        projects.projects2.forEach(function (project2) {
+            $(".work-row2").append('<div class="four columns"><a href="' + project2.link + '" target="_blank"><div class="imgWrap"><img src="' + project2.image + '" /><div class="imgDescription"><h4>' + project2.name + '</h4><p>' + project2.description + '</p></p></div></div></div></a>');
         });
     };
     projects.display();
